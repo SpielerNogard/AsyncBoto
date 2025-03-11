0cf73bf49b4c9ca8fa346b553b38c294f6410d32
Christopher Müller <christopher.mueller@be-storaged.com>
2025-03-11 09:10:33 +0100

    feat(async_boto.clients.lambda_): added AsyncLambdaClient

---
+++ async_boto/validation/lambda_/list_functions.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, Field, conint, constr
+from typing import Optional
+from typing import Literal
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from .data_types.function_configuration import FunctionConfiguration
+
+
+class ListFunctionsRequest(BaseModel):
+    """
+    Request model for the ListFunctions operation.
+
+    Returns a list of Lambda functions, with the version-specific configuration of each.
+    Lambda returns up to 50 functions per call.
+
+    Attributes
+    ----------
+    FunctionVersion : Optional[Literal["ALL"]]
+        Set to ALL to include entries for all published versions of each function.
+    Marker : Optional[str]
+        Specify the pagination token that's returned by a previous request to retrieve the next page of results.
+    MasterRegion : Optional[constr(regex=r'ALL|[a-z]{2}(-gov)?-[a-z]+-\d{1}')]
+        For Lambda@Edge functions, the AWS Region of the master function.
+    MaxItems : Optional[conint(ge=1, le=10000)]
+        The maximum number of functions to return in the response.
+    """
+
+    FunctionVersion: Optional[Literal["ALL"]] = None
+    Marker: Optional[str] = None
+    MasterRegion: Optional[constr(pattern=r"ALL|[a-z]{2}(-gov)?-[a-z]+-\d{1}")] = None
+    MaxItems: Optional[conint(ge=1, le=10000)] = None
+
+
+class ListFunctionsResponse(BaseModel):
+    """
+    Response model for the ListFunctions operation.
+
+    Attributes
+    ----------
+    Functions : List[FunctionConfiguration]
+        A list of Lambda functions.
+    NextMarker : Optional[str]
+        The pagination token that's included if more results are available.
+    """
+
+    Functions: List[FunctionConfiguration] = []
+    NextMarker: Optional[str] = None
